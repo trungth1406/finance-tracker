6e356db577a8d51e36a4cd4b0a7258500d22ac10
Trung Tran <trungth140694@gmail.com>
2020-08-21 00:03:57 +0700

    Add git ignore and Reader

---
+++ src-reader/main/java/com/self/learn/reader/ExportHandler.java
@@ -0,0 +1,34 @@
+package com.self.learn.reader;
+
+import com.self.learn.reader.io.base.IOHandler;
+
+import java.io.OutputStream;
+
+/**
+ *
+ */
+public class ExportHandler implements IOHandler {
+
+    /**
+     * Default constructor
+     */
+    public ExportHandler() {
+    }
+
+
+    /**
+     *
+     */
+    public OutputStream openStream() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     *
+     */
+    public void validateContent() {
+        // TODO implement here
+    }
+
+}
